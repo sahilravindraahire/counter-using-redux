5efb8be3a88e2242f0cc80844073261ca3fbbd7a
sahil ahire <sahilahire2003@gmail.com>
2026-04-09 15:15:35 +0530

    vhbfh

--- src/components/OnOffBtn.jsx
+++ src/components/OnOffBtn.jsx
@@ -9,7 +9,7 @@ function OnOffBtn() {
 
   return (
     <div>
-      <button onClick={() => dispatch(togglePower())} className={`px-6 py-2 mt-8 rounded-xl font-bold text-lg mb-4 transition-all duration-300 ${isOn ? "bg-green-500 text-white hover:bg-green-600": "bg-red-500 text-white hover:bg-red-600"}`}>
+      <button onClick={() => dispatch(togglePower())} className={`px-6 py-2 mt-0 rounded-b-full font-bold text-lg mb-4 transition-all duration-300 ${isOn ? "bg-green-500 text-white hover:bg-green-600": "bg-red-500 text-white hover:bg-red-600"}`}>
         {isOn ? "ON" : "OFF"}
       </button>
     </div>
