5d34cff69c453a9f89f62b12a6df843bf2c2656a
sahil ahire <sahilahire2003@gmail.com>
2026-04-09 15:10:15 +0530

    minor changes

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -11,9 +11,9 @@ function Buttons() {
 
   return (
     <div>
-      <button onClick={() => dispatch(decrease())} className={`text-2xl transition-all delay-300 mx-4 my-2 px-4 py-2 rounded-xl active:scale-95 active:bg-stone-900 hover:bg-stone-900 ${isOn ? "bg-black text-white" : "bg-zinc-700 text-zinc-700"}`}>Decrease</button>
-      <button onClick={() => dispatch(reset())} className={`text-2xl transition-all delay-300 mx-4 my-2 px-4 py-2 rounded-xl active:scale-95 active:bg-stone-900 hover:bg-stone-900 ${isOn ? "bg-black text-white" : "bg-zinc-700 text-zinc-700"}`}>Reset</button>
-      <button onClick={() => dispatch(increase())} className={`text-2xl transition-all delay-300 mx-4 my-2 px-4 py-2 rounded-xl active:scale-95 active:bg-stone-900 hover:bg-stone-900 ${isOn ? "bg-black text-white" : "bg-zinc-700 text-zinc-700"}`}>Increase</button>
+      <button onClick={() => dispatch(decrease())} className={`text-2xl transition-all delay-300 mx-4 my-2 px-4 py-2 rounded-xl active:scale-95 active:bg-stone-900 ${isOn ? "bg-black text-white hover:bg-stone-900" : "bg-zinc-700 text-zinc-700"}`}>Decrease</button>
+      <button onClick={() => dispatch(reset())} className={`text-2xl transition-all delay-300 mx-4 my-2 px-4 py-2 rounded-xl active:scale-95 active:bg-stone-900  ${isOn ? "bg-black text-white hover:bg-stone-900" : "bg-zinc-700 text-zinc-700"}`}>Reset</button>
+      <button onClick={() => dispatch(increase())} className={`text-2xl transition-all delay-300 mx-4 my-2 px-4 py-2 rounded-xl active:scale-95 active:bg-stone-900  ${isOn ? "bg-black text-white hover:bg-stone-900" : "bg-zinc-700 text-zinc-700"}`}>Increase</button>
     </div>
   )
 }
