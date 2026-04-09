8e3c5f97e87a3142050239c4ca38bcbb7b3f5647
sahil ahire <sahilahire2003@gmail.com>
2026-04-09 15:17:30 +0530

    nga

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Header from "./components/Header"
+
 import CounterDisplay from "./components/CounterDisplay"
 import Buttons from "./components/Buttons"
 import OnOffBtn from "./components/OnOffBtn"
@@ -10,7 +10,7 @@ function App() {
   return (
     <>
       <div className="h-screen flex flex-col justify-center items-center bg-slate-300">
-        <Header/>
+        
         <div className="px-4 py-2 bg-zinc-700 flex flex-col items-center justify-center rounded-xl mt-5">
           <OnOffBtn/>
           <CounterDisplay/>
